6b5b059aa537010184d553d8c8c025f8080fcb8f
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 22:52:32 +0530

    Update traffic_signal.c

--- traffic_signal.c
+++ traffic_signal.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 int main()
 {
+        char signal;
+    printf("Enter R,Y or G: ");
+    scanf("%c",&signal);
+        switch (signal)
+        {
+        case 'R':
+        case 'r': printf("STOP");
+                break;
+        case 'Y':
+        case 'y': printf("GET READY");
+                break;
+        case 'G': 
+        case 'g': printf("GO");
+                break;
+        default : printf("INVALID INPUT! RETRY.");
+            break;
+        }
 return 0;
 }
