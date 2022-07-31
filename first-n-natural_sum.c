f7dcbcc873b6e9993d2d4dd76550faacf2b52683
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 16:57:55 +0530

    Update first-n-natural_sum.c

--- first-n-natural_sum.c
+++ first-n-natural_sum.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main()
 {
+        int num,sum,i;
+    printf("Enter the number n: ");
+    scanf("%d",&num);
+        sum=0;
+    for (i = 1; i <= num; i++)
+    {
+        sum=sum+i;
+    }
+    printf("Summation of first %d natural numbers is %d.",num,sum);
+        
 return 0;
 }
