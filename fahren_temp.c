1b15a03f1a9e592f5854d4c2b00edab3f747755a
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 16:36:46 +0530

    Update fahren_temp.c

--- fahren_temp.c
+++ fahren_temp.c
@@ -1,5 +1,14 @@
+/*
+        FORMULA IS F=degC*(1.8)+32
+*/
+
 #include<stdio.h>
 int main()
 {
+        float c,fah;
+    printf("Enter value of deg Celsius: ");
+    scanf("%f",&c);
+        fah=c*(1.8)+32;
+    printf("%.2f deg Celsius is %.2f deg in Fahrenheit.",c,fah);
 return 0;
 }
