75b5a889dbb07c74ea9d5814f744835feb34f33f
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 17:24:05 +0530

    Update individual_digits_sum.c

--- individual_digits_sum.c
+++ individual_digits_sum.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 int main()
 {
+        int num,temp,digit,sum;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+            temp=num;   sum=0;
+        while(num != 0)
+        {
+            digit=num%10;
+            sum=sum+digit;
+            num=num/10;
+        }
+    printf("Summation of individual digits of %d is %d.",temp,sum);
 return 0;
 }
