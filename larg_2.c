181081751f63663505d17eda4cb4f6e69bd2dd7d
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 17:57:41 +0530

    Update larg_2.c

--- larg_2.c
+++ larg_2.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
+        int x,y;
+    printf("Enter two different values: ");
+    scanf("%d%d",&x,&y);
+        if(x>y)
+            printf("%d is largest.",x);
+        else
+            printf("%d is largest.",y);
 return 0;
 }
