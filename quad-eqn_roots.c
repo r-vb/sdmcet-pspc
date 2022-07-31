0e846d12e1ec2126c3e93c100f11b31db42b4ac7
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 19:50:29 +0530

    Update quad-eqn_roots.c

--- quad-eqn_roots.c
+++ quad-eqn_roots.c
@@ -1,5 +1,24 @@
-#include<stdio.h>
-int main()
-{
+#include <math.h>
+#include <stdio.h>
+int main() {
+    double a, b, c, discriminant, root1, root2;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    discriminant = (b * b) - (4 * a * c);
+    
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
+    }
+
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %.2lf;", root1);
+    }
+
+    else
+        printf("Roots are IMAGINARY.");
 return 0;
-}
+} 
