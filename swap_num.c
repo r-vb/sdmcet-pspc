e01b83795cb1c11ff53f6485149a40377f05d741
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 20:07:02 +0530

    Update swap_num.c

--- swap_num.c
+++ swap_num.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main()
 {
+        int a,b;
+    printf("Enter any two numbers to swap: ");
+    scanf("%d%d",&a,&b);
+    printf("Before swaping\na = %d\nb = %d",a,b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+    printf("\n\nAfter swaping\na = %d\nb = %d",a,b);
 return 0;
 }
