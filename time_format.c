2db065611638a89bae473acc3248c413e6c5c349
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 22:41:54 +0530

    Update time_format.c

--- time_format.c
+++ time_format.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+            int seconds, temp, hh, mm, ss;
+        printf("Enter value in seconds: ");
+        scanf("%d", &seconds);
+            temp = seconds / 60;
+            hh = temp / 60;
+            mm = temp % 60;
+            ss = seconds % 60;
+        printf("Time in HH-MM-SS format is %d:%d:%d", hh, mm, ss);
 return 0;
 }
