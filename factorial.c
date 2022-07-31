a1f4f2b3d97619faf5227598d4f4efceda8eb130
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 16:22:41 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 int main()
 {
+        int num,fact,temp;
+    printf("Enter the factorial number: ");
+    scanf("%d",&num);
+            temp=num;   fact=1;
+        if(num!=0)
+        {
+            while(num>0)
+            {
+                fact=fact*num;
+                num=num-1;
+            }
+            printf("Factorial of %d is %d.",temp,fact);
+        }
+        else
+            printf("Factorial of zero is 1",temp);
+        
 return 0;
 }
