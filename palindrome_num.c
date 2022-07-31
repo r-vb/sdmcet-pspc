ac2f907589aad266e7c7cad622187ecffde401ec
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 18:10:40 +0530

    Update palindrome_num.c

--- palindrome_num.c
+++ palindrome_num.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 int main()
 {
+        int num,temp,digit,reverse;
+    printf("Enter the number to check: ");
+    scanf("%d",&num);
+            temp=num;   reverse=0;
+        while (num != 0)
+        {
+            digit=num%10;
+            reverse=(reverse*10)+digit;
+            num=num/10;
+        }
+        if (temp == reverse)
+            printf("%d is a Palindrome number.",temp);
+        else
+            printf("%d is NOT a Palindrome number.",temp);
 return 0;
 }
