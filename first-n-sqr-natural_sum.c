9213d0e94415572b11f68f41bc181f7579196686
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 17:04:44 +0530

    Update first-n-sqr-natural_sum.c

--- first-n-sqr-natural_sum.c
+++ first-n-sqr-natural_sum.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main()
 {
+        int num,i,sum;
+        printf("Enter the value of n: ");
+        scanf("%d",&num);
+            sum=0;  i=1;
+        while(i<=num)
+        {
+            sum=sum+(i*i);
+            i++;
+        }
+        printf("Summation of squares of natural numbers upto %d is %d.",num,sum);
 return 0;
 }
