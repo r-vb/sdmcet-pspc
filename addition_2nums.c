c3f7759a06de904bac488cb64aa3b9bebe8c095a
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 13:15:06 +0530

    Update addition_2nums.c

--- addition_2nums.c
+++ addition_2nums.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main()
 {
+        int a,b,sum
+    printf("Enter any two numbers: ");
+    scanf("%d%d",&a,&b);
+        sum=a+b;
+    printf("Sum of the two numbers is %d",sum);
 return 0;
 }
