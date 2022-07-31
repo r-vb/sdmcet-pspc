734d0d778d5b81d03388c6fd6a503a36a5137c44
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 17:16:39 +0530

    Update gcd_lcm.c

--- gcd_lcm.c
+++ gcd_lcm.c
@@ -1,5 +1,22 @@
+//      USING EULER'S METHOD.
+
 #include<stdio.h>
 int main()
 {
+        int a,b,t1,t2,rem,gcd,lcm;
+    printf("Enter any two values: ");
+    scanf("%d%d",&a,&b);
+        t1=a;   t2=b;
+            rem=a%b;
+        while (b != 0)
+        {
+            rem=a%b;
+            a=b;
+            b=rem;
+        }
+            gcd=a;
+            lcm=(t1*t2)/gcd;
+        printf("GCD of %d and %d is %d.",t1,t2,gcd);
+        printf("\nLCM of %d and %d is %d.",t1,t2,lcm);
 return 0;
 }
