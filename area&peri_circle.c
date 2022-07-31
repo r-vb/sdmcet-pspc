ddd7fb48e5dd782c563f46417367bdfef57236bf
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 13:26:36 +0530

    Update area&peri_circle.c

--- area&peri_circle.c
+++ area&peri_circle.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+    int r;
+    float pi,peri,area;
+    printf("Enter the radius: ");
+    scanf("%d",&r);
+        pi=3.14;
+    peri=2*pi*r;
+    area=pi*r*r;
+    printf("Perimeter and area of the circle are %.3f and %.3f respectively.",peri,area);
 return 0;
 }
