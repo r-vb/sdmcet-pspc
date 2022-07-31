809bdb47664e7119b983858961effb23e4a572f4
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 13:21:42 +0530

    Update area_triangle.c

--- area_triangle.c
+++ area_triangle.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-return 0;
+        int a,b,c,s;
+        float area;
+    printf("Enter the value of three sides: ");
+    scanf("%d%d%d",&a,&b,&c);
+        s=(a+b+c)/2;
+        area=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("Area of the triangle is %.3f",area);
+    return 0;
 }
