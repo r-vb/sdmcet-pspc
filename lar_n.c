5265391b9325680398e20bc3b3f8a415193710d1
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 17:52:08 +0530

    Update lar_n.c

--- lar_n.c
+++ lar_n.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 int main()
 {
+        int i,num,x,large;
+    printf("How many numbers?\n");
+    scanf("%d",&x);
+        large=0;
+            for (i = 1; i <= x; i++)
+            {
+                printf("Enter %d number: ",i);
+                scanf("%d",&num);
+                    if(num>large)
+                    {
+                        large=num;
+                    }
+            }
+    printf("The largest number is %d.",large);
 return 0;
 }
