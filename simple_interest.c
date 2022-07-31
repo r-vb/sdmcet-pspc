037fc81a0efe3f0f68febaf183526e7d253667ea
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 19:55:44 +0530

    Update simple_interest.c

--- simple_interest.c
+++ simple_interest.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main()
 {
+        float p,t,r,si;
+    printf("Enter values of P,T and R respectively: ");
+    scanf("%f%f%f",&p,&t,&r);
+        si=p*t*r/100;
+    printf("Simple interest = %.2f",si);
 return 0;
 }
