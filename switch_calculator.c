5d25fbf7273352e360f4f83f846ee35a0b913144
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 20:47:05 +0530

    Update switch_calculator.c

--- switch_calculator.c
+++ switch_calculator.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 int main()
 {
+                float a,b;
+                char operator;
+        printf("Enter the two numbers: ");
+        scanf("%f%f",&a,&b);
+        printf("Enter required operator (+,-,*,/): ");
+        scanf(" %c", &operator);
+                switch (operator)
+                {
+                case '+': printf("Sum is %.4f", a+b);
+                        break;
+                case '-': printf("Difference is %.4f", a-b);
+                        break;
+                case '*': printf("Product is %.4f", a*b);
+                        break;
+                case '/': printf("Division gives %.4f", a/b);
+                        break;
+                default : printf("INVALID OPERAND! RETRY."); 
+                        break;
+                }
 return 0;
 }
