9d45e3dfcbdbca02483ab300655f26fc8a163f07
Rahul <91734971+r-vb@users.noreply.github.com>
2022-07-31 18:03:05 +0530

    Update larg_3.c

--- larg_3.c
+++ larg_3.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-return 0;
+    int a,b,c,lar;
+    printf("Enter three different values: ");
+    scanf("%d%d%d",&a,&b,&c);
+        lar=a;
+    if (lar<b)
+        lar=b;
+    if (lar<c)
+        lar=c;
+    printf("%d is largest among them.",lar);
+    return 0;
 }
