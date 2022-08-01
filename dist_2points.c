e94a1692e621b3eac8b8259afe499375a1631312
Rahul <91734971+r-vb@users.noreply.github.com>
2022-08-01 14:19:06 +0530

    Update dist_2points.c

--- dist_2points.c
+++ dist_2points.c
@@ -5,7 +5,7 @@ int main()
         int x1,x2,y1,y2;
         float dist;
     printf("Enter the coordinates of first point x1 and y1: ");
-    scanf("%d%d",&x1&y1);
+    scanf("%d%d",&x1,&y1);
     printf("Enter the coordinates of second point x2 and y2: ");
     scanf("%d%d",&x2,&y2);
         dist=sqrt(pow(x2-x1,2)+pow(y2-y1,2));
