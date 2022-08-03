cbdb57f82897eef6d6c4983679d79bdce1e3bbe0
Rahul <91734971+r-vb@users.noreply.github.com>
2022-08-03 17:59:26 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -3,7 +3,7 @@
 int main()
 {
         int terms,fib1=1,fib2=1,fib3,i;
-    printf("Enter the value of n terms: ");
+    printf("Enter the value of n: ");
     scanf("%d",&terms);
     printf("%d %d ",fib1,fib2);
         for ( i = 3; i <= terms; i++)
