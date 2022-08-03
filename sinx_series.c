4a1bdec7b714d23322625a9b064a8d3dd13dbd29
Rahul <91734971+r-vb@users.noreply.github.com>
2022-08-03 19:49:48 +0530

    Add files via upload

---
+++ sinx_series.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+        int i, n;
+        float x, sum, t;
+    printf(" Enter the value for x : ");
+    scanf("%f",&x);
+    printf(" Enter the value for n : ");
+    scanf("%d",&n);
+            x=x*(3.14159/180);
+            sum=x;  t=x;
+        for(i=1;i<=n;i++)
+        {
+            t=(t*(-1)*x*x)/(2*i*(2*i+1));
+            sum=sum+t;
+        }
+    printf(" The value of Sin(%f) = %.4f",x,sum);
+return 0;
+}
