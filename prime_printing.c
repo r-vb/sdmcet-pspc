43e4b8934d17d42127eb003c81d2857d874ad8a4
Rahul <91734971+r-vb@users.noreply.github.com>
2022-08-03 18:28:34 +0530

    Add files via upload

---
+++ prime_printing.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, rem, i, l, count;
+    printf("Enter starting and last numbers: ");
+    scanf("%d%d", &n1, &n2);
+    printf("Prime numbers from %d and %d are: ", n1, n2);
+    for (i = n1; i <= n2; i++)
+    {
+        count = 0;
+        for (l = 2; l <= i / 2; l++)
+        {
+            rem = i % l;
+            if (rem == 0)
+            {
+                count = 1;
+                break;
+            }
+        }
+        if (count == 0)
+            printf("%d ", i);
+    }
+    return 0;
+}
